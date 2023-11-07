2d40a29f30780400a04b388e406b3a7e59157f70
awtabish <awtabish.dev@gmail.com>
2023-11-07 19:47:04 +0500

    Level 2 Completed

---
+++ backend/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const mongoURI = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+
+const connectToMongo = () => {
+    mongoose.connect(mongoURI)
+    .then(()=> console.log("Connected to Mongo Successfully"))
+    .catch(err => console.log(err));
+}
+
+module.exports = connectToMongo;
